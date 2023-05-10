6c5fbf83d9f33d1bf5faa70d5ed0a9215fef92a9
LHPH <luislyly@hotmail.com>
2023-05-09 19:25:05 -0600

    upload project

---
+++ src/test/java/com/kyc/server/discovery/KycServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyc.server.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KycServerDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
